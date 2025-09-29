5426812d5cac35e459326968b2958353b74cc964
Muhammed Anas K <muhammedanask247@gmail.com>
2025-09-29 17:53:54 +0530

    ui for products created

--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -3,6 +3,7 @@
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useProductFilters } from "../../hooks/use-product-filters";
+import { ProductCard } from "./product-card";
 
 interface Props {
     category?: string;
@@ -21,10 +22,17 @@ export const ProductList = ({ category }: Props) => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
             {data?.docs.map((product) => (
-                <div key={product.id} className="border rounded-md bg-white p-4">
-                    <h2 className="text-xl font-medium">{product.name}</h2>
-                    <p>{product.price}</p>
-                </div>
+                <ProductCard 
+                    key={product.id}
+                    id={product.id}
+                    name={product.name}
+                    imageUrl={product.image?.url}
+                    authorUsername="anas"
+                    authorImageUrl={undefined}
+                    reviewRating={3}
+                    reviewCount={5}
+                    price={product.price}
+                />
             ))}
         </div>
     );
